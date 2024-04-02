7d7bb6923edd128a20975d42f5ccc07350207b0a
Pedro Henrique Bérgamo <pedroh.bergamo20@gmail.com>
2024-04-02 18:50:09 -0300

    ✨ feat: Aula 02 React Native

--- pass-in-react-native/src/app/index.tsx
+++ pass-in-react-native/src/app/index.tsx
@@ -3,9 +3,18 @@ import { Input } from "@/components/input";
 import { colors } from "@/styles/colors";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Link } from "expo-router";
-import { Image, StatusBar, View } from "react-native";
+import { useState } from "react";
+import { Alert, Image, StatusBar, View } from "react-native";
 
 export default function Home() {
+  const [code, setCode] = useState("");
+
+  function handleAccessCredentials() {
+    if (!code.trim()) {
+      return Alert.alert("Ingresso", "Informe o código do ingresso!");
+    }
+  }
+
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar barStyle="light-content" />
@@ -21,9 +30,12 @@ export default function Home() {
             color={colors.green[200]}
             size={20}
           />
-          <Input.Field placeholder="Código do ingresso" />
+          <Input.Field
+            placeholder="Código do ingresso"
+            onChangeText={setCode}
+          />
         </Input>
-        <Button title="Acessar credencial" />
+        <Button title="Acessar credencial" onPress={handleAccessCredentials} />
         <Link
           href="/register"
           className="text-gray-100 text-base font-bold text-center mt-8"
